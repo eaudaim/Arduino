1b8a0dcf235048cf5f829372f4674f8f036ccd11
eaudaim <82809613+eaudaim@users.noreply.github.com>
2021-11-21 16:21:49 +0100

    Add files via upload

---
+++ TempHumidityLCD.ino
@@ -0,0 +1,58 @@
+
+  //Le capteur
+
+#include "DHT.h"          // inclus la librairie DHT.h dans le code
+#define DHTPIN 3          // broche ou le capteur est branché
+#define DHTTYPE DHT11     // Model du capteur : DHT 11 
+DHT dht(DHTPIN, DHTTYPE); // Déclaration du capteur
+
+
+  //L'écran
+  
+#include <LiquidCrystal.h>              // Inclus la bibliothèque LiquideCrystal
+LiquidCrystal lcd(7, 8, 9, 10, 11, 12); //Déclaration des pins de l'écran
+
+void setup() {
+  Serial.begin(9600);
+  lcd.begin(16, 2);       // Initialise le nb de caractères et de lignes
+  dht.begin();
+}
+
+void loop() {
+  
+  delay(2000);                     // Délais entre chaque mesure
+  float h = dht.readHumidity();    //Lecture Humidité
+  float t = dht.readTemperature(); //Lecture Temperature
+
+
+
+  //Affichage LCD
+  
+  //Ligne 1  
+lcd.setCursor(0, 0);  //Met le curseur en colonne 1 et ligne 1
+lcd.print("Hum: ");   //Affiche sur le LCD la chaine de caractère "Hum: "
+lcd.setCursor(10, 0); //Met le curseur en colone 10 (ligne 1)
+lcd.print(h);         //Affiche la variable h
+lcd.setCursor(15, 0); //Met le curseur en colone 15 (ligne 1)
+lcd.print("%");       //Affiche le caractère "%"
+
+  //Ligne 2
+lcd.setCursor(0, 1);  //Met le curseur en colonne 1 et ligne 2
+lcd.print("Temp: ");  //Affiche sur le LCD la chaine de caractère "Temp: "
+lcd.setCursor(10, 1); //Met le curseur en colone 10 (ligne 2)
+lcd.print(t);         //Affiche la variable t
+lcd.setCursor(15, 1); //Met le curseur en colone 15 (ligne 2)
+lcd.print("C");       //Affiche le caractère "C"
+ 
+
+  //Affichage Serie
+
+ Serial.print("Temp = ");   //Affiche la chaine de caractère "Temp = " sur le port série
+ Serial.print(t);           //Affiche la variable t sur le port série
+ Serial.print(" °C Hum = ");//Affiche la chaine de caractère " °C Hum = " sur le port série
+ Serial.print(h);           //Affiche la variable h sur le port série
+ Serial.println(" %");      //Affiche la chaine de caractère " %" sur le port série
+
+delay(10000);               //Délais de 10 secondes
+} 
+  
